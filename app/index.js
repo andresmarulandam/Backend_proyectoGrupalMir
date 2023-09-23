1639a0ad9b830ed492359595e158b55498e7b4a1
Andres <marulandaandres19@gmail.com>
2023-09-22 20:34:38 -0500

    Add Request ID and Log System

--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
 import express from "express";
 import { router as api } from "./api/v1/index.js";
+import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
+import { HTTPlogger, logger } from "./logger.js";
 
 export const app = express();
+
+app.use((req, res, next) => {
+  const id = uuidv4();
+  req.id = id;
+  res.setHeader("X-Request-Id", id);
+
+  next();
+});
+
+// Log HTTP Requests
+app.use(HTTPlogger);
+
+// CORS
+app.use(cors());
 
 // Parse JSON body
 app.use(express.json());
@@ -20,6 +37,20 @@ app.use((req, res, next) => {
 // Error handler
 app.use((err, req, res, next) => {
   const { message = "", status = 500, error } = err;
+
+  const data = {
+    message,
+    status,
+    error,
+    traceId: req.id,
+    body: req.body,
+  };
+
+  if (status < 500) {
+    logger.warn(data);
+  } else {
+    logger.error(data);
+  }
 
   res.status(status);
   res.json({
